a51138ca94c591364a0c6f0a19d9105d88bcbc25
Artenks <artenkes@gmail.com>
2021-04-27 22:42:38 -0300

    v1.0-beta

---
+++ README.md
@@ -0,0 +1,11 @@
+# Gato-F5
+Aplicação Web para a cada reload da página, uma nvoa foto de gatinho aparecer.
+
+# *
+🟢Código Ok.
+
+🔴Código com problema.
+
+🟣Código atualizado.
+
+⚪Versão definitiva.
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="container">
+        <header className="homepage">
+          <div className="topbar"></div>
+          <div className="img-wrapper">
+            <img src="/img/gato1.jpg" alt="Gato imagem" />
+          </div>
+        </header>
+        <header className="midpage">
+          <h1>Gato</h1>
+        </header>
+        <div className="line-footpage"></div>
+        <header className="footpage">
+          <a href="https://twitter.com/Artenks"><li>Criado por Artenks 💚</li></a>
+
+        </header>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
